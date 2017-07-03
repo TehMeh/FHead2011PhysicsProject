1c65183abf29f93957fa7fb754d3abe6070a92c2
Simonas Drauksas <sdrauksa@lxplus071.cern.ch>
2017-07-03 16:20:48 +0200

    MakePrettyPlots.cpp and RunExample.cpp update 2017-07-03

--- JetShapeExploration/BasicJetPlots/20170623_ExamplePlots/MakePrettyPlots.cpp
+++ JetShapeExploration/BasicJetPlots/20170623_ExamplePlots/MakePrettyPlots.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
 #include "TFile.h"
@@ -9,6 +11,11 @@ using namespace std;
 
 int main()
 {
+   const int IDNum=4;
+   const int VarNum=8;
+   string IDName[IDNum+1]= {"Charged","Electron", "Muon", "Photon", "Hadron"};
+   string VarName[VarNum+1]={"Count", "PT", "E", "Eta", "Phi", "CountInJet", "DeltaR", "DeltaEta", "DeltaPhi"};
+  
    // Set plot style
    SetThesisStyle();
 
@@ -30,7 +37,36 @@ int main()
    PdfFile.AddHistogramFromFile(File, "HJetPhi", "", false);
    PdfFile.AddHistogramFromFile(File, "HJetEtaSmallPT", "", false);
    PdfFile.AddHistogramFromFile(File, "HJetPhiSmallPT", "", false);
-
+   PdfFile.AddHistogramFromFile(File, "HJetRawPTvsJetPT", "COLZ",false);
+   PdfFile.AddHistogramFromFile(File, "HPFPT", "", true);
+   PdfFile.AddHistogramFromFile(File, "HPFE", "", true);
+   PdfFile.AddHistogramFromFile(File, "HPFEta", "", false);
+   PdfFile.AddHistogramFromFile(File, "HPFPhi", "", false);
+ 
+   for (int i=0;i<=VarNum-4;i++){
+     for (int k=0;k<=IDNum;k++){
+       if (i==1 || i==2){
+	 PdfFile.AddHistogramFromFile(File, ("H"+IDName[k]+VarName[i]).c_str(), "", true);
+       }
+       else
+	 PdfFile.AddHistogramFromFile(File, ("H"+IDName[k]+VarName[i]).c_str(), "", false); 
+     }
+   }
+   for (int i=0;i<=IDNum;i++){
+     PdfFile.AddHistogramFromFile(File, ("H"+IDName[i]+VarName[5]).c_str(), "", false);
+   }
+   // DR,DEta,DPhi distribution pages
+   for (int k=VarNum-2;k<=VarNum;k++){
+     for (int i=0;i<=IDNum;i++){
+       for (int j=0;j<2;j++){
+	 if(j==0)
+	   PdfFile.AddHistogramFromFile(File, ("HJet"+IDName[i]+"PTvs"+VarName[k]).c_str(), "COLZ", true);
+	 else
+	   PdfFile.AddHistogramFromFile(File, ("HJet"+IDName[i]+"Countvs"+VarName[k]).c_str(), "", false);
+       }
+     }
+   }
+   PdfFile.AddHistogramFromFile(File, "HPTPercent", "", false);
    // Cleanup
    File.Close();
 
--- JetShapeExploration/BasicJetPlots/20170623_ExamplePlots/RunExample.cpp
+++ JetShapeExploration/BasicJetPlots/20170623_ExamplePlots/RunExample.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
 #include "TTree.h"
 #include "TFile.h"
+#include "TH1D.h"
 #include "TH2D.h"
 
 #include "ProgressBar.h"
@@ -61,15 +64,90 @@ int main(int argc, char *argv[])
    TH1D HJetPhi("HJetPhi", "JetPT > 50;Jet Phi;", 100, -M_PI, M_PI);
    TH1D HJetEtaSmallPT("HJetEtaSmallPT", "JetPT < 50;Jet Eta;", 100, -5, 5);
    TH1D HJetPhiSmallPT("HJetPhiSmallPT", "JetPT < 50;Jet Phi;", 100, -M_PI, M_PI);
+  
+   TH2D HJetRawPTvsJetPT("HJetRawPTvsJetPT","RawPT;PT",100,0,500,100,0,500);
+   TH1D HPFPT("HPFPT", ";PF PT", 100, 0, 60);
+   TH1D HPFE("HPFE", ";PF E", 100, 0, 60);
+   TH1D HPFEta("HPFEta" , ";PF Eta", 100, -5, 5);
+   TH1D HPFPhi("HPFPhi", ";PF Phi", 100,-M_PI, M_PI);
+   // Initiate necessary arrays and histograms 
+   const int IDNum=4;
+   const int VarNum=8;
+   // Parameters for Count: Charged, Electron, Muon, Photon, Hadron
+   const int Param[IDNum+1][3]= { {300, 0, 300}, {10, 0, 10}, {10, 0, 10}, {300, 0, 300}, { 60, 0 , 60} }; 
+   const double JetParamX[3][3]={ {100, 0, 0.4}, {100, -0.4, 0.4}, {100, -0.4, 0.4} };
+   string IDName[IDNum+1]= {"Charged","Electron", "Muon", "Photon", "Hadron"};
+   string PartonName[6]={"Up", "Down", "Strange", "Charm", "Bottom", "Gluon"};
+   string VarName[VarNum+1]={"Count", "PT", "E", "Eta", "Phi", "CountInJet", "DeltaR", "DeltaEta", "DeltaPhi"};
+   TH1D HPTPercent("HPTPercent", ";PTRatio", 100, 0, 1);
+   TH1D* HPTE=new TH1D("PTE", ";", 100, 0, 500);
+   TH1D* HCount=new TH1D("Count", ";", 1, 0 , 1);
+   TH1D* HEta=new TH1D("Eta", ";", 100, -5, 5);
+   TH1D* HPhi=new TH1D("Phi", ";", 100, -M_PI, M_PI);
+   TH2D* HJetDistrib= new TH2D("Jet", "", 1, 0, 1, 1, 0 , 1 );
+   std::vector<TH1D> vPT, vEta, vPhi, vEnergy, vCount, vCountInJet;
+   std::vector<std::vector<TH2D> > vPTDist;
+   std::vector<std::vector<TH1D> > vCountDist;
+   std::vector<TH2D> Temp2D;
+   std::vector<TH1D> Temp1D;
+   int IDCount[IDNum+1]={ 0, 0, 0, 0, 0};
+   int indx;
+   double DR,DEta,DPhi;
+   double PercentDR=0.1, PTRatio=0;
+   // making histogram vectors
+   for (int i=0;i<=IDNum;i++){
+     HCount->SetName(("H"+IDName[i]+VarName[0]).c_str());
+     HCount->SetTitle((";"+VarName[0]).c_str());
+     HCount->SetBins(Param[i][0], Param[i][1], Param[i][2]);
+     vCount.push_back(*HCount);
+     
+     HPTE->SetName(("H"+IDName[i]+VarName[1]).c_str());
+     HPTE->SetTitle((";"+VarName[1]).c_str());
+     vPT.push_back(*HPTE);
 
+     HPTE->SetName(("H"+IDName[i]+VarName[2]).c_str());
+     HPTE->SetTitle((";"+VarName[2]).c_str());
+     vEnergy.push_back(*HPTE);
+
+     HEta->SetName(("H"+IDName[i]+VarName[3]).c_str());
+     HEta->SetTitle((";"+VarName[3]).c_str());
+     vEta.push_back(*HEta);
+     
+     HPhi->SetName(("H"+IDName[i]+VarName[4]).c_str());
+     HPhi->SetTitle((";"+VarName[4]).c_str());
+     vPhi.push_back(*HPhi);
+
+     HCount->SetName(("HJet"+IDName[i]+VarName[5]).c_str());
+     HCount->SetTitle((";"+VarName[5]).c_str());
+     HCount->SetBins(Param[i][0], Param[i][1], Param[i][2]);
+     vCountInJet.push_back(*HCount);
+   }
+
+   // create 1D & 2D histograms for distributions DR, DEta, DPhi
+   for (int k=1; k<4;k++){
+       for (int i=0; i<=IDNum;i++){
+	 HJetDistrib->SetName(("HJet"+IDName[i]+"PTvs"+VarName[5+k]).c_str());
+	 HJetDistrib->SetTitle(("PT;"+VarName[5+k]).c_str());
+	 HJetDistrib->SetBins(JetParamX[k-1][0],JetParamX[k-1][1],JetParamX[k-1][2], 100, 0, 500 );
+	 Temp2D.push_back(*HJetDistrib);
+	 
+	 HCount->SetName(("HJet"+IDName[i]+"Countvs"+VarName[5+k]).c_str());
+	 HCount->SetTitle((";"+VarName[5+k]).c_str());
+	 HCount->SetBins(JetParamX[k-1][0],JetParamX[k-1][1], JetParamX[k-1][2]);
+	 Temp1D.push_back(*HCount);
+       }
+       vPTDist.push_back(Temp2D);
+       vCountDist.push_back(Temp1D);
+       Temp1D.clear();
+       Temp2D.clear();
+   }
    // Loop over events
    int EntryCount = MHiEvent.Tree->GetEntries() * 0.01;
 
    ProgressBar Bar(cout, EntryCount);
    Bar.SetStyle(-1);
-
    for(int iE = 0; iE < EntryCount; iE++)
-   {
+   {  
       // Progress bar :D
       Bar.Update(iE);
       Bar.PrintWithMod(200);
@@ -84,6 +162,9 @@ int main(int argc, char *argv[])
       // Fill histograms
       HN.Fill(0);
       HPTHat.Fill(MJet.PTHat);
+      for (int iJ=0; iJ<MJet.JetCount; iJ++){
+	HJetRawPTvsJetPT.Fill(MJet.JetRawPT[iJ],MJet.JetPT[iJ]);
+      }
 
       if(MJet.PTHat < PTHatMin || MJet.PTHat >= PTHatMax)
          continue;
@@ -92,6 +173,7 @@ int main(int argc, char *argv[])
 
       for(int iJ = 0; iJ < MJet.JetCount; iJ++)
       {
+	 
          HJetPT.Fill(MJet.JetPT[iJ]);
          if(MJet.JetPT[iJ] > 50)
          {
@@ -103,6 +185,60 @@ int main(int argc, char *argv[])
             HJetEtaSmallPT.Fill(MJet.JetEta[iJ]);
             HJetPhiSmallPT.Fill(MJet.JetPhi[iJ]);
          }
+	 //Jet selection and histogram filling
+	 if (MJet.JetPT[iJ]<=100 || abs(MJet.JetEta[iJ])>=1)
+	   continue;
+	 for (unsigned int i=0; i!=MPF.ID->size(); i++){
+	   DR=GetDR(MPF.Eta->at(i), MPF.Phi->at(i), MJet.JetEta[iJ], MJet.JetPhi[iJ]);
+	   if (DR>=0.4)
+	     continue;
+	   if (MPF.ID->at(i)>0 && MPF.ID->at(i)<6){
+	     DEta=MJet.JetEta[iJ]-MPF.Eta->at(i);
+	     DPhi=MJet.JetPhi[iJ]-MPF.Phi->at(i);
+	     if (DPhi<-M_PI) DPhi=DPhi+2*M_PI;
+	     else if (DPhi>M_PI) DPhi=DPhi-2*M_PI;
+	     indx=MPF.ID->at(i)-1;
+	   if (DR<PercentDR){
+		PTRatio=PTRatio+MPF.PT->at(i)/MJet.JetPT[iJ];
+	   }
+	     IDCount[indx]++;
+	       vPTDist.at(0).at(indx).Fill(DR,MPF.PT->at(i));
+	       vPTDist.at(1).at(indx).Fill(DEta,MPF.PT->at(i));
+	       vPTDist.at(2).at(indx).Fill(DPhi,MPF.PT->at(i));
+	     
+	       vCountDist.at(0).at(indx).Fill(DR);
+	       vCountDist.at(1).at(indx).Fill(DEta);
+	       vCountDist.at(2).at(indx).Fill(DPhi);
+	   }
+	 }
+	 HPTPercent.Fill(PTRatio);
+	 PTRatio=0;
+	 for (int k=0;k<=IDNum;k++){
+ 	   vCountInJet.at(k).Fill(IDCount[k]);
+	   IDCount[k]=0;
+     	 }
+	 
+      }
+     
+      for(unsigned int i=0; i!=MPF.ID->size(); i++){
+	HPFPT.Fill(MPF.PT->at(i));
+	HPFE.Fill(MPF.E->at(i));
+	HPFEta.Fill(MPF.Eta->at(i));
+	HPFPhi.Fill(MPF.Phi->at(i));
+       
+	
+	if (MPF.ID->at(i)-1>0 && MPF.ID->at(i)<6){
+	  indx=MPF.ID->at(i)-1;
+	  IDCount[indx]++;
+	  vPT.at(indx).Fill(MPF.PT->at(i));
+	  vEnergy.at(indx).Fill(MPF.E->at(i));
+	  vEta.at(indx).Fill(MPF.Eta->at(i));
+	  vPhi.at(indx).Fill(MPF.Phi->at(i));
+	}  
+      }
+      for (int k=0;k<=IDNum;k++){
+	vCount.at(k).Fill(IDCount[k]);
+	IDCount[k]=0;
       }
    }
 
@@ -120,8 +256,24 @@ int main(int argc, char *argv[])
    HJetPhi.Write();
    HJetEtaSmallPT.Write();
    HJetPhiSmallPT.Write();
-
-   // Cleanup
+   HPFPT.Write();
+   HPFE.Write();
+   HPFEta.Write();
+   HPFPhi.Write();
+  
+   for (int k=0;k<=IDNum;k++){
+     vCount.at(k).Write();
+     vPT.at(k).Write();
+     vEnergy.at(k).Write();
+     vEta.at(k).Write();
+     vPhi.at(k).Write();
+     for (int j=0;j<3;j++){
+       vPTDist.at(j).at(k).Write();
+       vCountDist.at(j).at(k).Write();
+     }
+   }
+   HPTPercent.Write();
+       // Cleanup
    InputFile->Close();
 
    return 0;
